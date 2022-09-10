574f600d4e275c14516ac00c288f66fafe681236
Samir Samawade <samirsamawade@gmail.com>
2022-09-10 12:06:34 +0300

    Server: Create Post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,14 @@
-export const PostsRoute = (req,res) =>{
-    res.send("Hello from Memories");
-};
+import Posts from "../models/posts.js";
 
-export const registerPost = (req,res) =>{
-    res.send("...Register");
+export const registerPost = async (req, res) => {
+  // const {title, description, budget} =  Posts
+  try {
+    const post = await new Posts({
+      title: req.body.title,
+      description: req.body.description, budget: req.body.budget
+    }).save();
+    res.json({ Status: true, Message: post });
+} catch (err) {
+      res.json({ Status: false, Message: err });
+  }
 };
--- server/routes/memories.js
+++ server/routes/memories.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { PostsRoute, registerPost } from '../controllers/posts.js';
+import { registerPost } from '../controllers/posts.js';
 const router = express.Router();
 
-router.get('/test', PostsRoute);
-router.get('/create-post', registerPost);
+router.post('/create-post', registerPost);
 
 
 export default router;
