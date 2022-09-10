72458ff17b2f4aebc48ca4b0c3a24f5eb0e969c7
Samir Samawade <samirsamawade@gmail.com>
2022-09-10 12:29:14 +0300

    Server: Create Post Destructuring

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,7 +2,8 @@ import Posts from "../models/posts.js";
 
 export const registerPost = async (req, res) => {
   try {
-    const post = await new Posts(req.body).save();
+    const {title, description, budget} = req.body;
+    const post = await new Posts({title, description, budget}).save();
     res.json({ Status: true, Message: post });
   } catch (err) {
     res.json({ Status: false, Message: err });
