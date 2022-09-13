e207c80980a0a55d8451ea36e8dccbd9d5fe5568
Samir Samawade <samirsamawade@gmail.com>
2022-09-13 11:48:36 +0300

    Client: Posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Posts from './components/Posts';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
-      Memories
+      <Posts/>
     </div>
   )
 }
---
+++ client/src/components/Posts.jsx
@@ -0,0 +1,7 @@
+const Posts = () => (
+    <div className="container">
+        <h1 className="text-center m-4 fs-1">Posts</h1>
+        </div>
+)
+
+export default Posts
