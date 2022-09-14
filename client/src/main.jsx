fa59a5e111e08273c348e562bf3f9da5622299a5
Samir Samawade <samirsamawade@gmail.com>
2022-09-14 16:34:41 +0300

    Client: Recoil Package Installed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import toast, { Toaster } from 'react-hot-toast';
+import { RecoilRoot } from 'recoil';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Toaster/>
-    <App />
+    <RecoilRoot>
+      <Toaster />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 )
