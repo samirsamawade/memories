57aa96a85bdb63ef2ef965e5604449bec144b7db
Samir Samawade <samirsamawade@gmail.com>
2022-09-14 11:58:16 +0300

    Client: Popup Form

--- client/src/components/ModalPopup.jsx
+++ client/src/components/ModalPopup.jsx
@@ -2,22 +2,36 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-const ModalPopup = ({show, setShow})=> (
-        <>           
-            <Modal show={show} onHide={()=>setShow(!show)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    
-                    <Button variant="primary" onClick={()=>setShow(!show)}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+const ModalPopup = ({ show, setShow }) => (
+    <>
+        <Modal show={show} onHide={() => setShow(!show)}>
+            <Modal.Header closeButton>
+                <Modal.Title>Manage Posts</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form className='my-4'>
+                    <div className="form-group my-2">
+                        <input type="text" className="text form-control " placeholder='Post title' />
+                    </div>
+                    <div className="form-group my-3">
+                        <textarea name="" rows="5" className="form-control" placeholder='Post description'></textarea>
+                    </div>
+                    <div className="form-group my-2">
+                        <input type="number" className="text form-control " placeholder='Post budget' />
+                    </div>
+
+                    <div className="form-group d-flex justify-content-end ">
+                        <Button variant="success" >
+                            Save Changes
+                        </Button>
+                    </div>
+
+                </form>
+            </Modal.Body>
+
+        </Modal>
+    </>
+);
 
 
 
